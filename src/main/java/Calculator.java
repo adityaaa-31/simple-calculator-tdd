9e314e786230c381a464b14cae2c461c5f7e9659
aditya shedge <adityavshedge3101@gmail.com>
2025-05-15 10:20:36 +0530

    add empty constructor in Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,6 +2,9 @@ public class Calculator {
     private int num1;
     private int num2;
 
+    public Calculator() {
+    }
+
     public Calculator(int num1, int num2) {
         this.num1 = num1;
         this.num2 = num2;
