55b21ae93cc95bcec62a43e27d974fd441c12de6
aditya shedge <adityavshedge3101@gmail.com>
2025-05-16 11:23:40 +0530

    add parse operands method

--- src/main/java/org/example/TokenParser.java
+++ src/main/java/org/example/TokenParser.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class TokenParser {
+    public int[] parseOperands(String[] operands) {
+        int[] convertedOperands = new int[2];
+        convertedOperands[0] = Integer.parseInt(operands[0]);
+        convertedOperands[1] = Integer.parseInt(operands[1]);
+
+        return convertedOperands;
+    }
 }
