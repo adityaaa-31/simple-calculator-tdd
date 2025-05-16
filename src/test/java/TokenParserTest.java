bbdcdaff98ccf04dc3d4a11cf8787a992f9c231c
aditya shedge <adityavshedge3101@gmail.com>
2025-05-16 11:19:20 +0530

    add test to parse int tokens

--- src/test/java/TokenParserTest.java
+++ src/test/java/TokenParserTest.java
@@ -1,4 +1,19 @@
+import org.example.TokenParser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TokenParserTest {
+    private TokenParser tokenParser = new TokenParser();
 
+    @Test
+    public void givenOperandsParseInt() {
+        String[] operands = new String[2];
+        operands[0] = "10";
+        operands[1] = "20";
+
+        int[] res = tokenParser.parseOperands(operands);
+        assertEquals(10, res[0]);
+        assertEquals(20, res[1]);
+    }
 }
