d122e2997c0a6e4c75eb0f795c6623e7325fd806
aditya shedge <adityavshedge3101@gmail.com>
2025-05-16 09:52:19 +0530

    add test to check the operator token

--- src/test/java/InputTokenizerTest.java
+++ src/test/java/InputTokenizerTest.java
@@ -1,8 +1,17 @@
 import org.example.InputTokenizer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InputTokenizerTest {
 
     private InputTokenizer inputTokenizer = new InputTokenizer();
 
+    @Test
+    public void givenInputStringCheckValidSumOperator() {
+        String input = "10 + 12";
+        String[] operator = inputTokenizer.getTokens(input);
+        assertEquals("+", operator[1]);
+    }
 
 }
