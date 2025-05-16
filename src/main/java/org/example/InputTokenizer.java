6e6500aa0d56f07f30f0f405a1a45396a156df68
aditya shedge <adityavshedge3101@gmail.com>
2025-05-16 11:25:01 +0530

    remove parsing method from input tokenizer

--- src/main/java/org/example/InputTokenizer.java
+++ src/main/java/org/example/InputTokenizer.java
@@ -6,11 +6,4 @@ public class InputTokenizer {
         return input.split(" ");
     }
 
-    public int[] parseOperands(String[] operands) {
-        int[] convertedOperands = new int[2];
-        convertedOperands[0] = Integer.parseInt(operands[0]);
-        convertedOperands[1] = Integer.parseInt(operands[1]);
-
-        return convertedOperands;
-    }
 }
