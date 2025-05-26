3cd70a28261915dd0dd67ac5aaabc06e1e9183db
aditya shedge <adityavshedge3101@gmail.com>
2025-05-26 15:28:02 +0530

    add method to parse operator

--- src/main/java/org/example/TokenParser.java
+++ src/main/java/org/example/TokenParser.java
@@ -3,9 +3,14 @@ package org.example;
 public class TokenParser {
     public int[] parseOperands(String[] operands) {
         int[] convertedOperands = new int[2];
+
         convertedOperands[0] = Integer.parseInt(operands[0]);
         convertedOperands[1] = Integer.parseInt(operands[1]);
 
         return convertedOperands;
+    }
+
+    public String parseOperator(String[] tokens) {
+        return tokens[1];
     }
 }
