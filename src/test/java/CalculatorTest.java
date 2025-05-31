59924eb5c9c2db9cad696edd4ae188dbed92bf2d
aditya shedge <adityavshedge3101@gmail.com>
2025-05-31 15:41:03 +0530

    update method input for calc test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -18,8 +18,10 @@ public class CalculatorTest {
     }
 
     public void givenSumOperationTypeReturnSum() {
-        String input = "10 + 20";
-        int res = calculator.calculate(input);
+        int num1 = 10;
+        int num2 = 20;
+
+        int res = calculator.calculate(OperationType.SUM, num1, num2);
         assertEquals(20, res);
     }
 
