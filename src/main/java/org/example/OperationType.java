e5ef6cc7ba23087dfa8a4503d3375a6b48c7218d
aditya shedge <adityavshedge3101@gmail.com>
2025-06-01 19:13:44 +0530

    update operation type enum to support string symbols

--- src/main/java/org/example/OperationType.java
+++ src/main/java/org/example/OperationType.java
@@ -1,8 +1,23 @@
 package org.example;
 
-    public enum OperationType {
-        SUM,
-        DIFFERENCE,
-        PRODUCT,
-        DIVISION
+import java.util.Arrays;
+
+public enum OperationType {
+        SUM("+"),
+        DIFFERENCE("-"),
+        PRODUCT("*"),
+        DIVISION("/");
+
+        private final String symbol;
+
+        OperationType(String symbol) {
+            this.symbol = symbol;
+        }
+
+        public static OperationType fromSymbol(String symbol) {
+            return Arrays.stream(values())
+                    .filter(op -> op.symbol.equals(symbol))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Invalid Operator"));
+        }
     }
